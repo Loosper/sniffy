e9a21e8b7cf32788c10119c259a83cb0407c544d
Loosper <boian4o1@gmail.com>
2018-06-15 00:23:28 +0300

    finally fixed

--- database.sql
+++ database.sql
@@ -4,34 +4,32 @@ CREATE DATABASE sniffy;
 USE sniffy;
 
 CREATE TABLE mac_address (
-    id INTEGER NOT NULL AUTO_INCREMENT,
-    address BINARY(6) NOT NULL PRIMARY KEY
+    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    address BINARY(6) NOT NULL
 );
 
 CREATE TABLE ipv4_address (
-    id INTEGER NOT NULL AUTO_INCREMENT,
-    address BINARY(6) NOT NULL PRIMARY KEY
+    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    address BINARY(6) NOT NULL
 );
 
 CREATE TABLE frame (
-    id INTEGER NOT NULL AUTO_INCREMENT,
+    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     source INTEGER NOT NULL,
     destination INTEGER NOT NULL,
     total INTEGER NOT NULL,
 
-    PRIMARY KEY (source, destination),
     FOREIGN KEY (source) REFERENCES mac_address(id),
     FOREIGN KEY (destination) REFERENCES mac_address(id)
 );
 
 CREATE TABLE packet (
-    id INTEGER NOT NULL AUTO_INCREMENT,
+    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     source INTEGER NOT NULL,
     destination INTEGER NOT NULL,
     total_valid INTEGER NOT NULL,
     total_invalid INTEGER NOT NULL,
 
-    PRIMARY KEY (source, destination),
 -- frame?
     FOREIGN KEY (source) REFERENCES ipv4_address(id),
     FOREIGN KEY (destination) REFERENCES ipv4_address(id)
@@ -42,7 +40,13 @@ CREATE TABLE arp_cache (
     mac INTEGER NOT NULL,
     ip INTEGER NOT NULL,
 
-    PRIMARY KEY (mac, ip),
     FOREIGN KEY (mac) REFERENCES mac_address(id),
     FOREIGN KEY (ip) REFERENCES ipv4_address(id)
 );
+
+CREATE UNIQUE INDEX mac_duplicate ON mac_address(address);
+CREATE UNIQUE INDEX ipv4_duplicate ON ipv4_address(address);
+
+CREATE UNIQUE INDEX frame_duplicate ON frame(source, destination);
+CREATE UNIQUE INDEX ipv4_duplicate ON ipv4(source, destination);
+CREATE UNIQUE INDEX arp_duplicate ON arp_cache(ip, mac);
