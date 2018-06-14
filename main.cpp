3b20c157f27000413301b7ac66fd2c4a8bda1b77
Viktor Naychev <vitonaychev@gmail.com>
2018-06-15 00:06:59 +0300

    connection established

--- main.cpp
+++ main.cpp
@@ -7,6 +7,14 @@
 
 #include <iostream>
 
+#include <mysql_connection.h>
+
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+
+
 #include "Connection.h"
 #include "helpers.h"
 
@@ -132,15 +140,36 @@ int main() {
     Connection conn = Connection();
     EthernetParser eth_parser = EthernetParser();
 
+    sql::Driver *driver;
+    sql::Connection *con;
+    sql::Statement *stmt;
+    sql::ResultSet *res;
+    sql::PreparedStatement *pstmt;
+
     // conn.bind_to_iface("wlp2s0");
-
-    while (1) {
-        uint8_t *frame = conn.recive();
-        eth_parser.parse(frame);
-
-        delete[] frame;
-        // break;
+    
+    try{
+        driver = get_driver_instance();
+        con = driver->connect("tcp://127.0.0.1:3306", "root", "root");
+        
+        con->setSchema("sniffy");
+        
+        while (1) {
+            uint8_t *frame = conn.recive();
+            eth_parser.parse(frame);
+            
+            delete[] frame;
+            // break;
+        }
+    }catch(sql::SQLException &e){
+        cout << "# ERR: SQLException in " << __FILE__;
+        cout << "(" << __FUNCTION__ << ") on line "\
+        << __LINE__ << endl;
+        cout << "# ERR: " << e.what();
+        cout << " (MySQL error code: " << e.getErrorCode();
+        cout << ", SQLState: " << e.getSQLState() << " )" << endl;
     }
+
 
     return 0;
 }
